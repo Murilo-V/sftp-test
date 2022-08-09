a87f2e2813dc45c3350ce7522f4aa0bf518c5ca6
Murilo-V <alveiramurilo@hotmail.com>
2022-08-09 11:27:39 -0300

    chore: .env

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+// const Client = require('ssh2-sftp-client');
+
+// const config = {
+//     host: 'example.com',
+//     port: 22,
+//     username: 'red-don',
+//     password: 'my-secret'
+//   };
+
+// const sftp = new Client();
+
+console.log(process.env.TESTE)
