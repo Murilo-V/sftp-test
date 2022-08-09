7f49fa48ff3e5c9ab01606a4c03bc0f86eb7d550
Murilo-V <alveiramurilo@hotmail.com>
2022-08-09 12:07:46 -0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 require('dotenv').config();
+const fs = require('fs');
 const Client = require('ssh2-sftp-client');
 
-const config = {
-    host: process.env.HOST,
+let config = {
     port: 22,
+    host: process.env.HOST,
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
+    passphrase: process.env.PASSPHRASE,
+    privateKey: fs.readFileSync(process.env.PRIVATE_KEY_DIR),
+    readyTimeout: 20000,
+    retries: 2,
+    retry_minTimeout: 2000,
   };
 
-const sftp = new Client();
+  class SFTPClient {
+    constructor() {
+      this.client = new Client();
+    }
+  
+    async connect(options) {
+      console.log(`Connecting to ${options.host}:${options.port}`);
+      try {
+        await this.client.connect(options);
+        console.log('conectou parcero')
+      } catch (err) {
+        console.log('Failed to connect:', err);
+      }
+    }
+  
+    async disconnect() {
+      await this.client.end();
+    }
+  
+  }
+
+  const client = new SFTPClient();
+client.connect(config);
